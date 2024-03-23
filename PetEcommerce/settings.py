9b447f5ae916de654eb5757d691c066a51dc7c23
Harandra <harandra04@gmail.com>
2024-03-23 20:19:16 +0530

    Deploying to Railway

--- PetEcommerce/settings.py
+++ PetEcommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-34s(4-@arojtep7q3xhd(rypnml8!v*oqw8$*@@!)k9k4$r(*6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PetEcommerce.wsgi --log-file -
